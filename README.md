5e59d142c68b1ba4569b3575666ca3edd746693c
pwszpl <szppszpsz@gmail.com>
2025-11-05 17:26:22 +0100

    Added original method to main class and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # puzzling-performance-solution
 Solution to https://www.kodewerk.com/java/performance/2025/09/28/puzzling-performance.html
+
+## Solutions
+Possible solutions available in Solutions class. If you want to solve it by yourself please do
+not look there ;)
+
+## To run
+1. Run main class in GenerateData class
+2. Run main class in CheckIntegerTestHarness class
+3. Feel free to modify checkInteger as you please to get the best results
--- src/main/java/com/kodewerk/profile/CheckIntegerTestHarness.java
+++ src/main/java/com/kodewerk/profile/CheckIntegerTestHarness.java
@@ -5,16 +5,6 @@ import java.io.*;
 
 public class CheckIntegerTestHarness {
     public static void main(String[] args) throws IOException {
-//        System.out.println("0".codePointAt(0));
-//        System.out.println("1".codePointAt(0));
-//        System.out.println("2".codePointAt(0));
-//        System.out.println("3".codePointAt(0));
-//        System.out.println("4".codePointAt(0));
-//        System.out.println("5".codePointAt(0));
-//        System.out.println("6".codePointAt(0));
-//        System.out.println("7".codePointAt(0));
-//        System.out.println("8".codePointAt(0));
-//        System.out.println("9".codePointAt(0));
         testDataset("dataset1.dat");
         testDataset("dataset2.dat");
         testDataset("dataset3.dat");
@@ -37,7 +27,7 @@ public class CheckIntegerTestHarness {
             long accumulatedTime = 0L;
             for (String testItem : testItems){
                 long starttime = System.currentTimeMillis();
-                boolean isInt = Solutions.checkInteger(testItem);
+                boolean isInt = checkInteger(testItem);
                 accumulatedTime += System.currentTimeMillis() - starttime;
                 if (isInt)
                     truecount++;
@@ -46,4 +36,17 @@ public class CheckIntegerTestHarness {
         }
     }
 
+    public static boolean checkInteger(String testInteger) {
+        try {
+            Integer theInteger = new Integer(testInteger);//fails if not  a number
+            return
+                    (theInteger.toString() != "") && //not empty
+                            (theInteger.intValue() > 10) && //greater than ten
+                            ((theInteger.intValue() >= 2) &&
+                                    (theInteger.intValue() <= 100000)) && //2>=X<=100000
+                            (theInteger.toString().charAt(0) == '3'); //first digit is 3
+        } catch (NumberFormatException err) {
+            return false;
+        }
+    }
 }
